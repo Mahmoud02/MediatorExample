ab44041c5db9d5105869a865cb7bca38a284b29a
captano <mahmoudreda027@gmail.com>
2021-02-27 20:47:09 +0200

    Update Colleague2.cs

--- MediatorExample/Structural/Colleague2.cs
+++ MediatorExample/Structural/Colleague2.cs
@@ -4,9 +4,7 @@ namespace Mediator.Structural
 {
     public class Colleague2 : Colleague
     {
-        //public Colleague2(Mediator mediator) : base(mediator)
-        //{
-        //}
+        
 
         public override void HandleNotification(string message)
         {
